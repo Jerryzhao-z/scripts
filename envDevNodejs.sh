c29f8a639b6ddd73946dcec116f0b8a220c9d3da
zhao <5126582@163.com>
2016-04-12 10:44:58 +0200

    install Nodejs

---
+++ envDevNodejs.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get remove nodejs
+sudo apt-get install git
+sudo apt-get install g++ curl make libssl-dev apache2-utils git-core
+cd 
+sudo git clone git://github.com/nodejs/node.git
+cd node
+./configure
+make
+sudo make install
+make test
+node -e "console.log('hello from Node.js' + process.version)"
